c80646f87460ad736c49bc22d773ebbca86ec7ab
Iamneo <techadmin@iamneo.ai>
2023-08-03 11:45:23 +0000

    Backup Commit

--- reactapp/src/functions/destructing.js
+++ reactapp/src/functions/destructing.js
@@ -5,7 +5,7 @@ return [userName, userIp];
 
 function getTopThree(studentMarks = []) {
     const [first=null, second=null, third=null, ...rest] = studentMarks;
-    return [first, second, third].Sort();
+    return [first, second, third].sort();
 }
 
 export {getUserIpDetails, getTopThree}
